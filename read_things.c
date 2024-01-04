7cbb61fbf66d9521c811f423c5e116d5eeb3362f
Md Ohiduzzaman Sumon <msumon@c2r1p9.42vienna.com>
2024-01-04 12:41:54 +0100

    OK

--- read_things.c
+++ read_things.c
@@ -6,7 +6,7 @@
 /*   By: msumon <msumon@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 20:17:43 by mhuszar           #+#    #+#             */
-/*   Updated: 2024/01/04 12:39:27 by msumon           ###   ########.fr       */
+/*   Updated: 2024/01/04 12:41:38 by msumon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	append_node(t_history **history, char *str)
 	t_history *entry;
 	t_history *proxy;
 
+	printf("Hello");
 	entry = malloc(sizeof(t_history));
 	entry->string = ft_strdup(str);
 	entry->next = NULL;
