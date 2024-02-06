e7daade29a09ac3032b56319d834fd9c4aef4622
Md Ohiduzzaman Sumon <msumon@c2r1p9.42vienna.com>
2024-02-06 14:55:01 +0100

    space upgrade implemented

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -6,11 +6,13 @@
 /*   By: msumon < msumon@student.42vienna.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 13:51:32 by mhuszar           #+#    #+#             */
-/*   Updated: 2024/02/06 11:07:28 by msumon           ###   ########.fr       */
+/*   Updated: 2024/02/06 14:49:43 by msumon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+char *ft_upgrade_spaces(char *line);
 
 int	entry_check(t_data *node, char *line)
 {
@@ -21,6 +23,7 @@ int	entry_check(t_data *node, char *line)
 	if (!ft_lexical_checker(line))
 		return (2);
 	line = ft_upgrade_spaces(line);
+	//printf("line: %s\n", line);
 	tokens = ft_calloc(sizeof(t_token *), pipe_counter(line));
 	if (!tokens)
 	{
@@ -54,32 +57,55 @@ char	**parse_input(char *line)
 	return (tokens);
 }
 
-char *ft_upgrade_spaces(line)
+char *ft_upgrade_spaces(char *line)
 {
 	//do the thing (except for " and ')
 	//free the line at the end because new version will replace it
+	int i;
+    int in_quotes;
+    char *result;
+    int j;
+	
+	i = 0;
+	j = 0;
+	in_quotes = 0;
+	result = malloc(strlen(line) * 3 + 1);
+	if (!result)
+	{
+		perror("Memory allocation failed");
+		exit(EXIT_FAILURE);
+	}
+    while (line[i])
+    {
+        if (line[i] == '"' || line[i] == '\'')
+            in_quotes = !in_quotes;
+        if (!in_quotes && (line[i] == '<' || line[i] == '>'))
+        {
+            result[j++] = ' ';
+            result[j++] = line[i];
+            if (line[i + 1] == line[i]) // Handle '<<' and '>>'
+            {
+                i++;
+                result[j++] = line[i];
+            }
+            result[j++] = ' ';
+        }
+        else
+        {
+            result[j++] = line[i];
+        }
+        i++;
+    }
+    result[j] = '\0';
+	free(line);
+    return (result);
 }
 
 
-// we have to iterate through line and implement the following here:
-	// 1. check if a ' or " has started,
-	//	they also have a maching sign that closes it
-	// 2. There are no two pipes following each other, like | | or
-	//	|| (we dont implement OR operator)
-	// 3. There are no two redirections following each other,
-	//	like > > or < < or >> >> etc. (BUT >> is okay)
-	// 4. there is no pipe without anything following it, like ls | ls |
-	// 5. there is no redirection without anything following it, like ls | ls  >
-	// 6. there should not be any spaces sorrounding a = sign
-	// 7. of course all of the above is allowed to happen inside quotes ("like this" 'or this')
-	// If any error of the above happens,
-	//	display "Syntax error near unexpected token", free line, return 0
-	// Otherwise: return 1
 int ft_lexer_error(char *line)
 {
 	(void)line;
 	printf("minishell: syntax error near unexpected token `newline'\n");
-	//free(line);
 	return (0);
 }
 
