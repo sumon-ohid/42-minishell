a0caf645db0b0976756711d44f884e55c729bdd6
sumon-ohid <smd-ohid@student.42wolfsburg.de>
2024-02-26 18:55:39 +0100

    echo  fixed

--- srcs/parsing/tokenize1.c
+++ srcs/parsing/tokenize1.c
@@ -24,7 +24,7 @@ char	*extract_var_name(char *str)
 	output = malloc(sizeof(char) * (ft_strlen_till_char(str, ' ') + 1));
 	if (!output)
 		handle_error("malloc in extract_var_name failed", 1);
-	while (str[i] && str[i] != ' ')
+	while (str[i] && str[i] != ' ' && str[i] != '$')
 	{
 		output[i] = str[i];
 		i++;
