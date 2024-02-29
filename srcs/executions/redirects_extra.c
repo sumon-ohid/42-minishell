5be613ca6a8d8b768266a1f345e656ce4733a00e
Maria Kamilla Huszar <mhuszar@c4r7p3.42vienna.com>
2024-02-29 14:14:18 +0100

    added fd freeing for failed dup2s in redirection part. fixed headers saying codespace

--- srcs/executions/redirects_extra.c
+++ srcs/executions/redirects_extra.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirects_extra.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 15:02:36 by msumon            #+#    #+#             */
-/*   Updated: 2024/02/22 12:04:56 by codespace        ###   ########.fr       */
+/*   Updated: 2024/02/29 14:13:07 by mhuszar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,10 @@ int	redirect_in(char *input, int mode, t_data *node)
 			return (0);
 	}
 	if (dup2(fd1, STDIN_FILENO) == -1)
+	{
+		ft_free_fds(node);
 		ft_exit(node, errno, "dup2 failed");
+	}
 	close(fd1);
 	return (1);
 }
@@ -51,7 +54,10 @@ int	redirect_out(char *output, int mode, t_data *node)
 			return (0);
 	}
 	if (dup2(fd2, STDOUT_FILENO) == -1)
+	{
+		ft_free_fds(node);
 		ft_exit(node, errno, "dup2 failed");
+	}
 	close(fd2);
 	return (1);
 }
@@ -73,7 +79,10 @@ int	redirect_out_append(char *output, int mode, t_data *node)
 			return (0);
 	}
 	if (dup2(fd2, STDOUT_FILENO) == -1)
+	{
+		ft_free_fds(node);
 		ft_exit(node, errno, "dup2 failed");
+	}
 	close(fd2);
 	return (1);
 }
