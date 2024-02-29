333bef1f44a0a3385388ee01b0fb1c53db862943
Maria Kamilla Huszar <mhuszar@c4r7p3.42vienna.com>
2024-02-29 20:15:32 +0100

    added newline after ctrl + C from cat

--- srcs/executions/cmd.c
+++ srcs/executions/cmd.c
@@ -6,7 +6,7 @@
 /*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 14:32:03 by msumon            #+#    #+#             */
-/*   Updated: 2024/02/29 19:03:44 by mhuszar          ###   ########.fr       */
+/*   Updated: 2024/02/29 20:13:58 by mhuszar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,8 @@ void	wait_for_processes(int *pid, int *status, int processes, t_data *node)
 		}
 		else
 		{
+			if (WIFSIGNALED(status[counter]) && counter == processes - 1)
+				write(STDOUT_FILENO, "\n", 1);
 			if (WIFEXITED(status[counter]) || WIFSIGNALED(status[counter]))
 				counter++;
 		}
