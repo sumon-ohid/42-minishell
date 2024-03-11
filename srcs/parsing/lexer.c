23d4589a9b27efd339eceb488287ee2dea5bbc07
sumon-ohid <smd-ohid@student.42wolfsburg.de>
2024-03-11 19:11:12 +0100

    lexer error more condition added

--- srcs/parsing/lexer.c
+++ srcs/parsing/lexer.c
@@ -6,7 +6,7 @@
 /*   By: msumon <msumon@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 13:51:32 by mhuszar           #+#    #+#             */
-/*   Updated: 2024/03/11 16:59:14 by msumon           ###   ########.fr       */
+/*   Updated: 2024/03/11 19:08:41 by msumon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	check_invalid_sequences(char c, char prev_char, char *line, int i)
 {
-	if (c == '|' && prev_char == '|')
+	if (c == '|' && line[i + 1] == ' ' && line[i + 2] == '|')
 		return (ft_lexer_error(line));
 	else if (c == '<' && prev_char == '>')
 		return (ft_lexer_error(line));
@@ -23,6 +23,10 @@ int	check_invalid_sequences(char c, char prev_char, char *line, int i)
 	else if (c == '|' && !prev_char)
 		return (ft_lexer_error(line));
 	else if (c == '|' && prev_char == '>')
+		return (ft_lexer_error(line));
+	else if (c == '<' &&  line[i + 1] == ' ' && line[i + 2] == '|')
+		return (ft_lexer_error(line));
+	else if (c == '>' &&  line[i + 1] == ' ' && line[i + 2] == '|')
 		return (ft_lexer_error(line));
 	else if (c == '|' && prev_char == '<')
 		return (ft_lexer_error(line));
@@ -37,6 +41,12 @@ int	check_invalid_endings(char c, char *line, int i)
 {
 	if (c == '>' && line[i + 1] == '\0')
 		return (ft_lexer_error(line));
+	else if (c == '<' && line[i + 1] == ' ' && line[i + 2] == '\0')
+		return (ft_lexer_error(line));
+	else if (c == '>' && line[i + 1] == ' ' && line[i + 2] == '\0')
+		return (ft_lexer_error(line));
+	else if (c == '|' && line[i + 1] == ' ' && line[i + 2] == '\0')
+		return (ft_lexer_error(line));
 	else if (c == '<' && line[i + 1] == '\0')
 		return (ft_lexer_error(line));
 	else if (c == '|' && line[i + 1] == '\0')
@@ -45,23 +55,14 @@ int	check_invalid_endings(char c, char *line, int i)
 		return (ft_lexer_error(line));
 	else if (c == ')' && line[i + 1] == '\0')
 		return (ft_lexer_error(line));
-	return (1);
-}
-
-int	check_invalid_next_chars(char c, char *line, int i)
-{
-	if (c == '>' && line[i + 1] == ' ' && !(line[i + 2] >= 'A' && line[i
-				+ 2] <= 'z'))
+	else if (c == '<' && line[i + 1] == ' ' && line[i + 2] == '<')
 		return (ft_lexer_error(line));
-	else if (c == '<' && line[i + 1] == ' ' && !(line[i + 2] >= 'A' && line[i
-				+ 2] <= 'z'))
-		return (ft_lexer_error(line));
-	else if (c == '=' && line[i + 1] == ' ')
+	else if (c == '>' && line[i + 1] == ' ' && line[i + 2] == '>')
 		return (ft_lexer_error(line));
 	return (1);
 }
 
-char	*remove_spaces(char *input, t_data *node)
+char	*remove_spaces_keep_one(char *input, t_data *node)
 {
 	int		i;
 	int		j;
@@ -69,22 +70,19 @@ char	*remove_spaces(char *input, t_data *node)
 
 	i = 0;
 	j = 0;
-	line = malloc(ft_strlen(input) + 1);
+	line = malloc(sizeof(char) * ft_strlen(input) + 1);
 	if (!line)
+		ft_exit(node, 1, "malloc failed in remove_spaces_keep_one");
+	while (input[i])
 	{
-		free(input);
-		ft_free_array(node->envp);
-		free(node);
-		handle_error("malloc failed at lexer", -1);
-	}
-	while (input[i] != '\0')
-	{
-		if (input[i] != ' ')
+		if (input[i] == ' ' && input[i + 1] == ' ')
+			i++;
+		else
 		{
 			line[j] = input[i];
+			i++;
 			j++;
 		}
-		i++;
 	}
 	line[j] = '\0';
 	return (line);
@@ -98,7 +96,7 @@ int	ft_lexical_checker(t_data *node, int in_single_quote, int in_double_quote,
 	char	*line;
 
 	i = 0;
-	line = remove_spaces(node->input_line, node);
+	line = remove_spaces_keep_one(node->input_line, node);
 	while ((line[i]) != '\0')
 	{
 		c = line[i];
