b7e2a5ad36099f00ded747900b92a8052e2d9b39
sumon-ohid <smd-ohid@student.42wolfsburg.de>
2024-03-12 17:52:02 +0100

    unset with quotes fixed

--- srcs/builtins/ft_unset.c
+++ srcs/builtins/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: msumon <msumon@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 19:16:45 by msumon            #+#    #+#             */
-/*   Updated: 2024/03/05 11:24:31 by msumon           ###   ########.fr       */
+/*   Updated: 2024/03/12 17:50:59 by msumon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	ft_unsetenv(t_data *node, char *str)
 {
-	int		i;
-	int		j;
+	int	i;
+	int	j;
 
 	i = 0;
 	while (node->envp[i])
@@ -40,14 +40,19 @@ int	ft_unset(t_data *node, t_token *token, char *str)
 {
 	int		i;
 	char	**vars;
+	char	*new_str;
 
 	(void)token;
-	vars = ft_split(str, ' ', 0, 0);
+	new_str = remove_quote(str);
+	if (new_str == NULL)
+		return (1);
+	vars = ft_split(new_str, ' ', 0, 0);
 	if (vars == NULL)
 	{
 		ft_putstr("ft_split failed at ft_unset function\n");
 		return (1);
 	}
+	free(new_str);
 	i = 1;
 	while (vars[i])
 	{
