169aa22e3e7edea4e7f312ae7f266284d71897bd
sumon-ohid <smd-ohid@student.42wolfsburg.de>
2024-03-13 12:51:08 +0100

    pwd fixed for inside dir remove out dir

--- srcs/builtins/ft_pwd.c
+++ srcs/builtins/ft_pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_pwd.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
+/*   By: msumon <msumon@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 09:25:54 by msumon            #+#    #+#             */
-/*   Updated: 2024/02/29 18:36:35 by mhuszar          ###   ########.fr       */
+/*   Updated: 2024/03/13 12:50:00 by msumon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,14 @@
 int	ft_pwd(t_data *node)
 {
 	char	*pwd;
+	char	*tmp;
 
 	pwd = getcwd(NULL, 0);
 	if (!pwd)
 	{
-		ft_putstr_fd("minishell: pwd: No such file or directory\n", 2);
+		tmp = ft_getenv("PWD",node);
+		ft_putstr_fd(tmp, 2);
+		ft_putstr_fd("\n", 2);
 		node->last_return = -99;
 		return (1);
 	}
