df5a529eb0ab42825bf052870d6ff250df9c06a5
sumon-ohid <smd-ohid@student.42wolfsburg.de>
2024-03-13 12:53:22 +0100

    ok

--- srcs/builtins/ft_pwd.c
+++ srcs/builtins/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: msumon <msumon@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 09:25:54 by msumon            #+#    #+#             */
-/*   Updated: 2024/03/13 12:50:00 by msumon           ###   ########.fr       */
+/*   Updated: 2024/03/13 12:53:00 by msumon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_pwd(t_data *node)
 	pwd = getcwd(NULL, 0);
 	if (!pwd)
 	{
-		tmp = ft_getenv("PWD",node);
+		tmp = ft_getenv("PWD", node);
 		ft_putstr_fd(tmp, 2);
 		ft_putstr_fd("\n", 2);
 		node->last_return = -99;
